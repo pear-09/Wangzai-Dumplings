783e2c25123a6b4ca5c85fed645e8a24cbb2469b
littlyellowduck <145242110+littlyellowduck@users.noreply.github.com>
2024-11-12 22:28:31 +0800

    创建前端vue框架 (#29)

---
+++ vue3-work/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <UserHeader />
+</template>
+
+<script lang="ts">
+import UserHeader from "./components/UserHeader.vue";
+import request from "./utils/request.ts"
+  export default {
+    name:'App',
+    components:{UserHeader}
+  }
+</script>
+
+<style scoped>
+
+</style>
