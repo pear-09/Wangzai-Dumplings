997ba91806cfa859a78153ca268a02f8bb034bfe
黎曼 <2901868597@qq.com>
2024-11-13 18:33:53 +0800

    写前端开发以及测试框架与流程

---
+++ vue3-work/src/types/api/getTasks.d.ts
@@ -0,0 +1,18 @@
+// src/types/api/getTasks.d.ts
+// 该接口用于获取单天日程
+
+// 定义日程类型
+export interface Task {
+    id: number;
+    title: string;
+    description: string;
+    time: string;
+    status: string;
+  }
+  
+  // 获取日程列表接口的响应类型
+  export interface GetTasksResponse {
+    code: number;
+    msg: string;
+    data: Task[];
+  }
