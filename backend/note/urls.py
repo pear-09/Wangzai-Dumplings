390ce908e8dec7e83b649f29bb762a6df4c841c9
gsgqh <128687770+gsgqh@users.noreply.github.com>
2024-11-15 23:18:39 +0800

    笔记删除接口 (#39)
    * 部分register接口
    
    * 修复注册接口问题，加入登陆接口
    
    * 获取文件夹内所有笔记接口
    
    * 笔记删除接口
    
    * merge

--- backend/note/urls.py
+++ backend/note/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('update/content', views.update_note_view, name='update/content'),
     path('query',views.get_note_view, name='query'),
     path('query-all',views.get_notes_in_folder_view, name='query-all'),
+    path('delete',views.delete_note_view, name='delete'),
 ]
