12b9eac996b67fd9eb616c0104cf05f4f733f19b
Ricky-chen1 <Ricky__chen@outlook.com>
2024-11-15 21:44:40 +0800

    doc: add requirement.txt

--- backend/note/forms.py
+++ backend/note/forms.py
@@ -7,6 +7,10 @@ class CreateNoteForm(forms.ModelForm):
         model = Note
         fields = ['title', 'content', 'folder_id', 'tags']
 
+     # 设置字段的默认值和空值处理
+    title = forms.CharField(required=False, initial="新建笔记")  # 默认值为 "新建笔记"
+    content = forms.CharField(required=False, widget=forms.Textarea)  # 内容字段可选
+
 class UpdateNoteForm(forms.ModelForm):
     note_id = forms.IntegerField(required=True, error_messages={'required': 'note_id 不能为空'})
     content = forms.CharField(required=True, error_messages={'required': 'content 不能为空'})
