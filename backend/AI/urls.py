bc00ca7cdc05152e92596641579bdf098e3acb9d
gsgqh <298668170@qq.com>
2024-11-25 21:53:58 +0800

    AI部分功能

--- backend/AI/urls.py
+++ backend/AI/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('summary', views.ai_summary, name='summary'),
+    path('explain', views.ai_explain, name='explain'),
+    path('keywords', views.ai_extract_keywords, name='keywords'),
+    path('translate', views.ai_translate, name = 'translate'),
+    path('plan', views.ai_plan, name = 'plan'),
+    path('beauty', views.ai_beauty, name = 'beauty')
 ]
